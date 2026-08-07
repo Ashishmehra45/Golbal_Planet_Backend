bc81c53529371a52c6354b65df446bb0d33396cd
Ashish Mehra <ashish6266mehra@gmail.com>
2026-08-07 15:14:42 +0530

    werfgth

--- route/productRoutes.js
+++ route/productRoutes.js
@@ -6,6 +6,7 @@ const upload = require('../middleware/upload');
 // Map routes to controller functions
 router.post('/create', upload.single('image'), createProduct); // Create a new product
 router.get('/getAllProducts', getProducts); // Get all products
+
 router.get('/:id', getProductById); // Get a single product by ID
 
 module.exports = router;
