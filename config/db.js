141aedb5a312a3cf65029d2ed7663ea9a38ec497
Ashish Mehra <ashish6266mehra@gmail.com>
2026-08-07 12:54:15 +0530

    all done

--- config/db.js
+++ config/db.js
@@ -1,12 +1,17 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const dns = require("dns");
+
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
 const connectDB = async () => {
   try {
+    console.log("URI:", process.env.MONGODB_URI);
+
     const conn = await mongoose.connect(process.env.MONGODB_URI);
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
-  } catch (error) {
-    console.error(`Error: ${error.message}`);
-    // process.exit(1); // Exit process with failure
+
+    console.log("MongoDB Connected:", conn.connection.host);
+  } catch (err) {
+    console.error(err);
   }
 };
 
